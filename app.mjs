a689395c690c80ee02a281388df0e4166cbc9a54
Alex <alex4jdm@gmail.com>
2022-09-24 17:00:09 +0300

    Added packages and .env.

--- app.mjs
+++ app.mjs
@@ -1,15 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import db from './models/index.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-app.use(bodyParser.json()); 
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(bodyParser.json()); 
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.post('/user', (req, res) => {
+  
 })
 
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 })
