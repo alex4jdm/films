fe2196063d8ba838c7396da9d6496959912020ff
Alex <alex4jdm@gmail.com>
2022-09-25 03:14:24 +0300

    Added actors migration.

--- migrations/20220925001131-create-actor.js
+++ migrations/20220925001131-create-actor.js
@@ -1,25 +1,17 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('Actors', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      email: {
+      name: {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true
-      },
-      name: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      password: {
-        allowNull: false,
-        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
@@ -32,6 +24,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Actors');
   }
 };
