bcda7eaa60db2ecc90f2dd6051894b08ae4090b3
Alex <alex4jdm@gmail.com>
2022-09-25 21:58:42 +0300

    Added token checking for movies routes.

--- src/v1/routes/movie.router.mjs
+++ src/v1/routes/movie.router.mjs
@@ -1,14 +1,20 @@
 import Router from 'express';
+import dotenv from 'dotenv';
+import { expressjwt } from 'express-jwt';
 import { MovieController } from '../controllers/index.mjs';
 
+dotenv.config();
 const movieController = new MovieController();
 const router = Router();
+const secret = process.env.AUTH_KEY;
+const algorithms = ['HS256'];
+const jwtOpt = { secret, algorithms };
 
-router.post('/', movieController.createMovie.bind(movieController));
-router.delete('/:id', movieController.deleteMovie.bind(movieController));
-router.patch('/:id', movieController.updateMovie.bind(movieController));
-router.get('/:id', movieController.getSingle.bind(movieController));
-router.get('/', movieController.getMany.bind(movieController));
-router.post('/import', movieController.importMovies.bind(movieController));
+router.post('/', expressjwt(jwtOpt), movieController.createMovie.bind(movieController));
+router.delete('/:id', expressjwt(jwtOpt), movieController.deleteMovie.bind(movieController));
+router.patch('/:id', expressjwt(jwtOpt), movieController.updateMovie.bind(movieController));
+router.get('/:id', expressjwt(jwtOpt), movieController.getSingle.bind(movieController));
+router.get('/', expressjwt(jwtOpt), movieController.getMany.bind(movieController));
+router.post('/import', expressjwt(jwtOpt), movieController.importMovies.bind(movieController));
 
 export default router;
